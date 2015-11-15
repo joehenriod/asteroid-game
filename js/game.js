4c8355c3e74ff216a0994e23bf8166d176c66bbd
Joe Henriod <joehenriod@gmail.com>
2015-11-15 21:21:51 +0000

    loading graphic assests

--- js/game.js
+++ js/game.js
@@ -7,6 +7,15 @@ var states = {
     game: "game",
 };
 
+var graphicAssets = {
+    ship:{URL:'assets/ship.png', name:'ship'},
+    bullet:{URL:'assets/bullet.png', name:'bullet'},    
+    
+    asteroidLarge:{URL:'assets/asteroidLarge.png', name:'asteroidLarge'},
+    asteroidMedium:{URL:'assets/asteroidMedium.png', name:'asteroidMedium'},
+    asteroidSmall:{URL:'assets/asteroidSmall.png', name:'asteroidSmall'},
+};
+
 var gameState = function(game){
     
 };
