4daaabe604a6f632d4f53c714893c269d2f2e703
Joe Henriod <joehenriod@gmail.com>
2015-11-17 05:46:57 +0000

    preload graphics

--- js/game.js
+++ js/game.js
@@ -23,7 +23,12 @@ var gameState = function(game){
 gameState.prototype = {
     
     preload: function () {
+        game.load.image(graphicAssets.asteroidLarge.name, graphicAssets.asteroidLarge.URL);
+        game.load.image(graphicAssets.asteroidMedium.name, graphicAssets.asteroidMedium.URL);
+        game.load.image(graphicAssets.asteroidSmall.name, graphicAssets.asteroidSmall.URL);
         
+        game.load.image(graphicAssets.bullet.name, graphicAssets.bullet.URL);
+        game.load.image(graphicAssets.ship.name, graphicAssets.ship.URL);
     },
     
     create: function () {
