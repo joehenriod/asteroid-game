a7542abbf67a25ac42b18747676e651dc56b6968
Joe Henriod <joehenriod@gmail.com>
2015-11-19 04:35:10 +0000

    check boundaries

--- js/game.js
+++ js/game.js
@@ -90,3 +90,17 @@ gameState.prototype = {
 var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
 game.state.add(states.game, gameState);
 game.state.start(states.game);
+
+checkBoundaries: function (sprite) {
+        if (sprite.x < 0) {
+            sprite.x = game.width;
+        } else if (sprite.x > game.width) {
+            sprite.x = 0;
+        } 
+ 
+        if (sprite.y < 0) {
+            sprite.y = game.height;
+        } else if (sprite.y > game.height) {
+            sprite.y = 0;
+        }
+    },
